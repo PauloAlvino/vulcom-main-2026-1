56d1db4b31b1ab7f4f13920f5991ef4fc2d460dc
Pauloricardo1808 <pr243140@gmail.com>
2026-04-25 11:32:40 -0300

    (25/04) Configuração de CORS e hashing de senhas

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,26 +1,35 @@
-import dotenv from 'dotenv'
-dotenv.config() // Carrega as variáveis de ambiente do arquivo .env
+import dotenv from "dotenv";
+dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
 
-import express, { json, urlencoded } from 'express'
-import cookieParser from 'cookie-parser'
-import logger from 'morgan'
+import express, { json, urlencoded } from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
-const app = express()
+const app = express();
 
-app.use(logger('dev'))
-app.use(json())
-app.use(urlencoded({ extended: false }))
-app.use(cookieParser())
+import cors from "cors";
+
+app.use(
+  cors({
+    origin: process.env.ALLOWED_ORIGINS.split(","),
+    // credentials: true
+  }),
+);
+
+app.use(logger("dev"));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
 
 /*********** ROTAS DA API **************/
 
-import carsRouter from './routes/cars.js'
-app.use('/cars', carsRouter)
+import carsRouter from "./routes/cars.js";
+app.use("/cars", carsRouter);
 
-import customersRouter from './routes/customers.js'
-app.use('/customers', customersRouter)
+import customersRouter from "./routes/customers.js";
+app.use("/customers", customersRouter);
 
-import usersRouter from './routes/users.js'
-app.use('/users', usersRouter)
+import usersRouter from "./routes/users.js";
+app.use("/users", usersRouter);
 
-export default app
+export default app;
